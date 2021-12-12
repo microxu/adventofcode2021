79bf6a7c27c9bad89ec13db2cd812849b90e0381
microxu <microxu@sina.com>
2021-12-12 13:44:15 +1300

    refactoring

--- myadvent/src/main/java/myadvent/day11.java
+++ myadvent/src/main/java/myadvent/day11.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class day11 {
 
@@ -17,11 +18,9 @@ public class day11 {
 	public int rows=0;
 	public int cols=0;
 	public int flashs=0;
-    public int[][] statusOneDay;
     
     public void firstOne() {
         for(int i=0;i<100;i++) {
-        	initStatus();
         	allPointsAfterOneDay(); 	
         }
     }
@@ -30,23 +29,28 @@ public class day11 {
     	int r=0;
     	while(checkStatus()) {
     		r=r+1;
-    		initStatus();
     		allPointsAfterOneDay(); 
     	}
     	return r;
     }
     
     private void allPointsAfterOneDay() {
-    	for(int x=0;x<this.points.size();x++) {
-    		for(int y=0;y<this.points.get(x).length;y++) {
-    			List<int[]> t=Arrays.asList(new int[] {x,y});
-    			aPonitAndSurroundingAfterOneDay(t);
+    	oneday();
+    	List<int[]> c=copy();
+    	for(int x=0;x<c.size();x++) {
+    		for(int y=0;y<c.get(x).length;y++) {
+    			if(c.get(x)[y]==10) {
+    				List<int[]> t=Arrays.asList(new int[] {x,y});
+    				aPonitAndSurroundingAfterOneDay(t);
+    			}
     		}
-    	}  	
+    	} 
+    	reset();
     }
     
 	private void aPonitAndSurroundingAfterOneDay(List<int[]> p){
 		for (int[] point : p) {
+			this.flashs+=1;
 			List<int[]> temp=onePointAfterOneDay(point);
 			if(temp.size()>0) {
 				aPonitAndSurroundingAfterOneDay(temp);
@@ -57,154 +61,112 @@ public class day11 {
 	private List<int[]> onePointAfterOneDay(int[] point) {
 		int x= point[0];
 		int y=point[1];
-		int value=this.points.get(x)[y];
-		boolean flash=false;
+
 		List<int[]> r=new ArrayList<int[]>();
-		if(value<10) 
-			points.get(x)[y]=value+1;
-				
-		if(points.get(x)[y]==10 && statusOneDay[x][y]!=1) {
-			statusOneDay[x][y]=1;
-			flash=true;
-			this.flashs=this.flashs+1;
-		}
 		
 		//up
 		if(x-1>=0) {
-			if(flash) {
-				if(this.points.get(x-1)[y]==9 && statusOneDay[x-1][y]==0) {
+			if(this.points.get(x-1)[y]<10) {
+				this.points.get(x-1)[y]=this.points.get(x-1)[y]+1;
+				if(this.points.get(x-1)[y]==10 ) {
 					r.add(new int[] {x-1,y});
-					statusOneDay[x-1][y]=2;
-				}else {
-					//not flashed
-					if(this.points.get(x-1)[y]<10) {
-						this.points.get(x-1)[y]=this.points.get(x-1)[y]+1;
-					}
 				}
 			}
 		}
+		
 		//down
 		if(x+1<rows) {
-			if(flash) {
-				if(this.points.get(x+1)[y]==9 && statusOneDay[x+1][y]==0) {
+			if(this.points.get(x+1)[y]<10) {
+				this.points.get(x+1)[y]=this.points.get(x+1)[y]+1;
+				if(this.points.get(x+1)[y]==10 ) {
 					r.add(new int[] {x+1,y});
-					statusOneDay[x+1][y]=2;
-				}else {
-					if(this.points.get(x+1)[y]<10) {
-						this.points.get(x+1)[y]=this.points.get(x+1)[y]+1;
-					}
 				}
 			}
-			
-		}		
+		}	
+		
 		//left
 		if(y-1>=0) {
-			if(flash) {
-				if(this.points.get(x)[y-1]==9 && statusOneDay[x][y-1]==0) {
+			if(this.points.get(x)[y-1]<10) {
+				this.points.get(x)[y-1]=this.points.get(x)[y-1]+1;
+				if(this.points.get(x)[y-1]==10 ) {
 					r.add(new int[] {x,y-1});
-					statusOneDay[x][y-1]=2;
-				}else {
-					if(this.points.get(x)[y-1]<10) {
-						this.points.get(x)[y-1]=this.points.get(x)[y-1]+1;
-					}
-				}
-					
-			}		
-		}
-		//right
-		if(y+1<cols) {
-			if(flash) {
-				if(this.points.get(x)[y+1]==9 && statusOneDay[x][y+1]==0) {
-					r.add(new int[] {x,y+1});
-					statusOneDay[x][y+1]=2;
-				}else {
-					if(this.points.get(x)[y+1]<10) {
-						this.points.get(x)[y+1]=this.points.get(x)[y+1]+1;
-					}
-				}
-			}		
-		}
-		//up+left
-		if((x-1)>=0 && (y-1)>=0) {
-			if(flash) {
-				if(this.points.get(x-1)[y-1]==9 && statusOneDay[x-1][y-1]==0) {
-					r.add(new int[] {x-1,y-1});
-					statusOneDay[x-1][y-1]=2;
-				}else {
-					if(this.points.get(x-1)[y-1]<10) {
-						this.points.get(x-1)[y-1]=this.points.get(x-1)[y-1]+1;
-					}
 				}
 			}
 		}
+		
+		//right
+		if(y+1<cols) {
+			if(this.points.get(x)[y+1]<10) { 
+				this.points.get(x)[y+1]=this.points.get(x)[y+1]+1;
+				if(this.points.get(x)[y+1]==10 ) {
+					r.add(new int[] {x,y+1});
+				}
+			}
+		}
+		
+		//up+left
+		if((x-1)>=0 && (y-1)>=0) {
+			if(this.points.get(x-1)[y-1]<10) { 
+				this.points.get(x-1)[y-1]=this.points.get(x-1)[y-1]+1;
+				if(this.points.get(x-1)[y-1]==10) {
+					r.add(new int[] {x-1,y-1});
+				}
+			}
+		}
+		
 		//down+left
 		if((x+1)<rows && (y-1)>=0) {
-			if(flash) {
-				if(this.points.get(x+1)[y-1]==9 && statusOneDay[x+1][y-1]==0) {
+			if(this.points.get(x+1)[y-1]<10) { 
+				this.points.get(x+1)[y-1]=this.points.get(x+1)[y-1]+1;
+				if(this.points.get(x+1)[y-1]==10) {
 					r.add(new int[] {x+1,y-1});
-					statusOneDay[x+1][y-1]=2;
-				}else {
-					if(this.points.get(x+1)[y-1]<10) {
-						this.points.get(x+1)[y-1]=this.points.get(x+1)[y-1]+1;
-					}
 				}
 			}
 		}
 		//up+right
 		if((x-1)>=0 && (y+1)<cols) {
-			if(flash) {
-				if(this.points.get(x-1)[y+1]==9 && statusOneDay[x-1][y+1]==0) {
+			if(this.points.get(x-1)[y+1]<10) {
+				this.points.get(x-1)[y+1]=this.points.get(x-1)[y+1]+1;
+				if(this.points.get(x-1)[y+1]==10 ) {
 					r.add(new int[] {x-1,y+1});
-					statusOneDay[x-1][y+1]=2;
-				}else {
-					if(this.points.get(x-1)[y+1]<10) {
-						this.points.get(x-1)[y+1]=this.points.get(x-1)[y+1]+1;
-					}
 				}
 			}
 		}
 		//down+right
 		if((x+1)<rows && (y+1)<cols) {
-			if(flash) {
-				if(this.points.get(x+1)[y+1]==9 && statusOneDay[x+1][y+1]==0) {
+			if(this.points.get(x+1)[y+1]<10) {
+				this.points.get(x+1)[y+1]=this.points.get(x+1)[y+1]+1;
+				if(this.points.get(x+1)[y+1]==10 ) {
 					r.add(new int[] {x+1,y+1});
-					statusOneDay[x+1][y+1]=2;
-				}else {
-					if(this.points.get(x+1)[y+1]<10) {
-						this.points.get(x+1)[y+1]=this.points.get(x+1)[y+1]+1;
-					}
 				}
 			}
 		}
 		return r;
 	}
 	
-	public void initStatus() {
-		this.statusOneDay=new int[rows][cols];
-		for(int i=0;i<this.rows;i++) {
-			for(int j=0;j<this.cols;j++) {
-				this.statusOneDay[i][j]=0;
-				if(this.points.get(i)[j]==10) {
-					this.points.get(i)[j]=0;
-				}
-			}
-		}
+	private void reset() {
+		this.points=this.points.stream().map(m->Arrays.stream(m).map(
+				(int n) ->{
+				   if(n==10)
+					   return n=0;
+				   else
+					   return n;
+				}).toArray()).collect(Collectors.toList());
 	}
-	public boolean checkStatus() {
-		boolean r=false;
-    	for(int x=0;x<this.points.size();x++) {
-    		for(int y=0;y<this.points.get(x).length;y++) {
-    			if(this.points.get(x)[y]!=10) {
-    				r=true;
-    				break;
-    			}
-    			if(r) {
-    				break;
-    			}
-    		}
-    	} 
-		return r;
+	
+	private void oneday() {
+    	this.points=this.points.stream().map(m->Arrays.stream(m).map((int n) ->n+1).toArray()).collect(Collectors.toList());
 	}
+	
+	private List<int[]> copy(){
+		return this.points.stream().map(m-> Arrays.copyOf(m, m.length)).collect(Collectors.toList());
+
+	}
+	
+	private boolean checkStatus() {    	
+		return this.points.stream().filter(m->Arrays.stream(m).anyMatch(n->n!=0)).count() >0? true:false;
+	}
+	
 	public void getInput() throws IOException {
 		File file = new File("src/main/resources/input11.txt");
 		InputStream in = new FileInputStream(file);
@@ -237,10 +199,13 @@ public class day11 {
     	
     	day11 d =new day11();	
     	d.getInput();
-    	//d.firstOne();
+//    	
+//    	d.firstOne();
+//   	System.out.println(d.flashs);
+    	
     	int r=d.secondOne();
-    	//System.out.println(d.flashs);
     	System.out.println(r);
+    	
         long endTime = System.currentTimeMillis(); //done
         System.out.println("running time:" + (endTime - startTime) + "ms"); //running time
 
